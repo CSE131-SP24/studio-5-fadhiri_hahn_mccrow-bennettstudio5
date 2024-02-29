937d6e1f6f44fb0ebb8e141141a3fdd3cbb3e77f
jamesahahn <jamesahahn@localhost>
2024-02-29 12:52:04 -0600

    Studio 5!

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -1,5 +1,7 @@
 package studio5;
 
+import static org.junit.Assert.assertEquals;
+
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Methods {
@@ -15,10 +17,21 @@ public class Methods {
 	 */
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
 		double distance = 0;
+		double distance1 = (x2 - x1) * (x2 - x1);
+		double distance2 = (y2 - y1) * (y2 - y1);
+		distance = Math.sqrt((distance1)+(distance2));
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
 		
 		return distance;
-	}
+		
+		//assertEquals(0.0, // The value we expect to get (to pass the test)
+		//Methods.distanceBetween(1.0, 0.0, 1.0, 0.0), // The thing to test
+		//1e-5); // This uses double, which are imprecise, so
+				// the delta is used to decide how close is "close enough".
+				// In this case + or - 0.00001 is being used
+}
+
+	
 
 	/**
 	 * Draw a bull's eye at the given location with the given radius.
@@ -30,19 +43,24 @@ public class Methods {
 	public static void drawBullsEye(double x, double y, double radius) {
 		StdDraw.setPenColor(StdDraw.BLACK);
 		StdDraw.filledCircle(x, y, radius);
-
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
+		StdDraw.setPenColor(StdDraw.BLUE);
+		StdDraw.filledCircle(x, y, 3.0/4.0*radius);
 		// suggested rgb values: 0, 109, 219
 
 		
 
 		// Red ring with 1.0/2.0 the radius
+		StdDraw.setPenColor(StdDraw.RED);
+		StdDraw.filledCircle(x, y, 1.0/2.0*radius);
 		// suggested rgb values: 146, 0, 0
 
 		
 
 		// Yellow ring with 1.0/4.0 the radius
+		StdDraw.setPenColor(StdDraw.ORANGE);
+		StdDraw.filledCircle(x, y, 1.0/4.0*radius);
 		// suggested rgb values: 255, 255, 109
 
 		
